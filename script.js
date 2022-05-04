61f7e567de0c3b029b8e1d33bb9586b360e271dd
Julian Lindsay-Kaufman <jjjuliannnn456@gmail.com>
2022-05-04 18:03:42 -0400

    adjusted gameArea dimensions, button click resize effect, added js funcs to change the title and button innerHTML along with changes in count

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 let count = 0;
 const button = document.getElementById('button')
 const currentCount = document.getElementById('currentCount')
+const startTitle = document.getElementById('startTitle')
+const tenTitle = document.getElementById('tenTitle')
+
 
 button.addEventListener('click', () => {
     count +=1
-    currentCount.innerHTML = count
-    if (button.innerHTML !== 'Keep clicking!') button.innerHTML='Keep clicking!'
+    if (currentCount.innerHTML = "Click me to start playing!") currentCount.innerHTML = `Clicks: ${count}`
+
+    if (count === 10) {
+        startTitle.style.display = 'none';
+        tenTitle.style.display = 'flex';
+        currentCount.innerHTML = 'I want to know! (10)'
+    }
+
+    if (count === 11) {
+        tenTitle.style.display = 'none';
+        currentCount.innerHTML = `Clicks: ${count}`
+    }
+})
+
+button.addEventListener('mousedown', () => {
+    button.style.transform = 'scale(0.9)'
+})
+
+button.addEventListener('mouseup', () => {
+    button.style.transform = 'scale(1)'
 })
