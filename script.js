cbbc7a232d1bd3b053b60e25f30794bdd2e34e52
Julian Lindsay-Kaufman <jjjuliannnn456@gmail.com>
2022-05-04 15:17:16 -0400

    improvements to styling - wrapper div, font family, spacing, button design and hover effect, hr element

--- script.js
+++ script.js
@@ -5,4 +5,5 @@ const currentCount = document.getElementById('currentCount')
 button.addEventListener('click', () => {
     count +=1
     currentCount.innerHTML = count
+    if (button.innerHTML !== 'Keep clicking!') button.innerHTML='Keep clicking!'
 })
