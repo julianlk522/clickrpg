44f6cb37cf097b72d3b249e08eb708c0f9ef336c
Julian Lindsay-Kaufman <jjjuliannnn456@gmail.com>
2022-05-18 20:08:04 -0400

    working countdown clock but trying to fix bug of game not ending when timer hits 0

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
-let count = 0;
+//  game variables
+let count = 1
+let totalSecondsLeft = 300
+
+// dom selectors
 const button = document.getElementById('button')
 const currentCount = document.getElementById('currentCount')
 const startTitle = document.getElementById('startTitle')
-const tenTitle = document.getElementById('tenTitle')
+const secondsLeftElement = document.getElementById('secondsLeft')
+const timeLeftElement = document.getElementById('timeLeft')
 
+//  handle timer
+function handleTimer() {
+    setInterval(() => {
+        //  game over if no time left
+        if (totalSecondsLeft - 1 < 0) {
+            totalSecondsLeft = 0
+            console.log('Game over!')
+            clearInterval(secondClock)
+        }
 
+        //  update seconds left html
+        totalSecondsLeft--
+        secondsLeftElement.innerHTML = totalSecondsLeft
+
+        //  minutes and seconds calcs
+        let minutes = Math.floor(totalSecondsLeft / 60)
+        let seconds = (totalSecondsLeft - minutes * 60) % 60
+        timeLeftElement.innerHTML = `${minutes} : ${seconds}`
+    }, 1000)
+}
+
+//  button click handler
 button.addEventListener('click', () => {
-    count +=1
-    if (currentCount.innerHTML = "Click me to start playing!") currentCount.innerHTML = `Clicks: ${count}`
-
-    if (count === 10) {
-        startTitle.style.display = 'none';
-        tenTitle.style.display = 'flex';
-        currentCount.innerHTML = 'I want to know! (10)'
-    }
-
-    if (count === 11) {
-        tenTitle.style.display = 'none';
+    if ((currentCount.innerHTML = 'Click me to start playing!')) {
         currentCount.innerHTML = `Clicks: ${count}`
     }
+    if (count < 2) {
+        handleTimer()
+    }
+    count += 1
 })
 
+//  button hover effects
 button.addEventListener('mousedown', () => {
     button.style.transform = 'scale(0.9)'
 })
