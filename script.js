09be6f3128e99e5e4a388c1c6804bca6c566f18f
Julian Lindsay-Kaufman <jjjuliannnn456@gmail.com>
2022-05-20 09:06:37 -0400

    fixed timer to start smoothly with no delay, game over function, timer now removes subtitle and hr element when the game starts, optimized spacing of title/hr/timer elements during different game phases

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 //  game variables
-let count = 1
-let totalSecondsLeft = 60
+let count = 0
+let gameTimeSeconds = 5
 
 // dom selectors
-const button = document.getElementById('button')
-const currentCount = document.getElementById('currentCount')
-const startTitle = document.getElementById('startTitle')
+const buttonElement = document.getElementById('button')
+const currentCountElement = document.getElementById('currentCount')
+const subTitleElement = document.getElementById('subtitle')
 const timerElement = document.getElementById('timer')
+const hrElement = document.querySelector('hr')
+const gameOverElement = document.getElementById('gameOver')
 
 //  calc time in mins/secs and update html
 const newTimeCalcs = () => {
 	//  minutes and seconds calcs
-	let minutes = Math.floor(totalSecondsLeft / 60)
-	let seconds = totalSecondsLeft % 60
+	let minutes = Math.floor(gameTimeSeconds / 60)
+	let seconds = gameTimeSeconds % 60
 	seconds = seconds < 10 ? '0' + seconds : seconds
-
+	
 	//  update html during game
 	timerElement.innerHTML = `${minutes} : ${seconds}`
 }
@@ -22,8 +24,8 @@ const newTimeCalcs = () => {
 // parse time left and return formatted start message
 const buildStartTimeMessage = () => {
 	//  starting minutes and seconds calcs
-	let startingMinutes = Math.floor(totalSecondsLeft / 60)
-	let startingSeconds = totalSecondsLeft % 60
+	let startingMinutes = Math.floor(gameTimeSeconds / 60)
+	let startingSeconds = gameTimeSeconds % 60
 
 	const startTimeMessage = !startingMinutes
 		? startingSeconds === 1
@@ -40,7 +42,6 @@ const buildStartTimeMessage = () => {
 				: `${startingMinutes} minutes ${startingSeconds} seconds until time is up!`
 
 	timerElement.innerHTML = startTimeMessage
-	console.log(startingSeconds)
 }
 
 //  load starting time
@@ -48,40 +49,53 @@ window.onload = () => {
 	buildStartTimeMessage()
 }
 
+//	game over
+const gameOver = () => {
+	gameOverElement.innerHTML = `Game over!  Your score was ${count}!`
+	gameOverElement.style.display = 'block'
+}
+
 //  handle timer
 function handleTimer() {
+	newTimeCalcs()
+	subTitleElement.style.display = 'none'
+	hrElement.style.display = 'none'
+	
 	const timerInterval = setInterval(() => {
+		//  decrement totalSecondsLeft
+		gameTimeSeconds--
+
 		//  game over if no time left
-		if (totalSecondsLeft <= 0) {
-			totalSecondsLeft = 0
+		if (gameTimeSeconds <= 0) {
+			gameTimeSeconds = 0
 			timerElement.innerHTML = '0 : 00'
 			console.log('Game over!')
+			gameOver()
 			return clearInterval(timerInterval)
 		}
-
+		
 		newTimeCalcs()
-
-		//  decrement totalSecondsLeft
-		totalSecondsLeft--
 	}, 1000)
 }
 
 //  button click handler
-button.addEventListener('click', () => {
-	if ((currentCount.innerHTML = 'Click me to start playing!')) {
-		currentCount.innerHTML = `Clicks: ${count}`
-	}
-	if (count < 2) {
+buttonElement.addEventListener('click', () => {
+	if (count === 0) {
 		handleTimer()
 	}
+
 	count += 1
+
+	if ((currentCountElement.innerHTML = 'Click me to start playing!')) {
+		currentCountElement.innerHTML = `Clicks: ${count}`
+	}
 })
 
 //  button hover effects
-button.addEventListener('mousedown', () => {
-	button.style.transform = 'scale(0.9)'
+buttonElement.addEventListener('mousedown', () => {
+	buttonElement.style.transform = 'scale(0.9)'
 })
 
-button.addEventListener('mouseup', () => {
-	button.style.transform = 'scale(1)'
+buttonElement.addEventListener('mouseup', () => {
+	buttonElement.style.transform = 'scale(1)'
 })
